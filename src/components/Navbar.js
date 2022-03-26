401ca7676976a1757eb538dad0a7c174e30b3fc6
amalsabu59 <amalsabu59@gmail.com>
2022-03-26 20:30:35 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Button = styled.button `
+  color: grey;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+        navbar
+    </Container>
+        
+    
+  )
+}
+
+export default Navbar
