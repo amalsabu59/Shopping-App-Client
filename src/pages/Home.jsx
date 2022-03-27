10ec276f9c2e21c4d4a39bdbded5dde225f7e8ef
amalsabu59 <amalsabu59@gmail.com>
2022-03-27 21:49:42 +0530

    pages created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
 import Slider from '../components/Slider'
 
+
 const Home = () => {
   return (
     <div>
@@ -17,7 +18,7 @@ const Home = () => {
         <Products />
         <Newsletter/>
         <Footer/>
-
+        
     </div>
   )
 }
